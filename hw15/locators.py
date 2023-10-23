2d51fcfeb262c8632c0d4370b86011c9206f9187
BOPOH <mospan.yulia@gmail.com>
2023-10-23 16:52:30 +0300

    hw15 - done

---
+++ hw15/locators.py
@@ -0,0 +1,57 @@
+"""https://resiliencehub.com.ua/"""
+
+locators_xpath_1 = ("//button[contains(@class, 'button_buttonPrimary__0TFIN button_button__X2akn header-profile-menu_"
+                    "header__registerBtn__kmB9C')]")
+locators_xpath_2 = "//div[@class='header_header__item__D0BvQ']"
+locators_xpath_3 = "//*[@class='button_buttonLink__lyWZx button_button__X2akn specialist-section_link__PrTDO']"
+locators_xpath_4 = "/html/body/div/div[1]/div[4]/div/button"
+locators_xpath_5 = "//button[last()-1]"
+locators_xpath_6 = "(//button)[5]"
+locators_xpath_7 = ("//div[@class='header_header__item__D0BvQ']/following-sibling::div[@class='header_header__item__"
+                    "D0BvQ']")
+locators_xpath_8 = ("//*[@class='specialist-section_button__WEFmb']/ancestor::section[@class='specialist-section_"
+                    "section__pyLTK']")
+locators_xpath_9 = ("//*[@class='button_buttonPrimaryYellow___D48t button_button__X2akn become-specialist-section_"
+                    "becomeSpecialist__button__bBdIv']/ancestor::div[@class='become-specialist-section_becomeSpecialist"
+                    "__3X8Ya']")
+locators_xpath_10 = "//div[@class='become-specialist-section_becomeSpecialist__3X8Ya']"
+locators_xpath_11 = "//img[@src='/_next/static/media/img.dba5f507.png']"
+locators_xpath_12 = "//div[@class='index_faqScreen__title__Y_iDs']"
+locators_xpath_13 = ("//div[@class='index_faqScreen__title__Y_iDs']/following-sibling::div[@class='index_faqScreen__"
+                     "btnWrapper__6A__n']")
+locators_xpath_14 = ("//div[@class='index_faqScreen__title__Y_iDs']/following-sibling::div[@class='index_faqScreen__"
+                     "btnWrapper__6A__n']/button")
+locators_xpath_15 = ("//div[@class='index_faqScreen__btnWrapper__6A__n']/preceding-sibling::div[@class='index_"
+                     "faqScreen__title__Y_iDs']")
+locators_xpath_16 = ("//div[@class='index_faqScreen__btnWrapper__6A__n']/preceding-sibling::div[@class='index_"
+                     "faqScreen__title__Y_iDs']/ancestor::div[@class='index_faqScreen__HWam2']")
+locators_xpath_17 = "//*[@src='/_next/static/media/hand-heart.babae8c1.svg']"
+locators_xpath_18 = "//*[@class='footer_upSection__title__EtnHK']"
+locators_xpath_19 = "//*[contains(@class,'footer_upSection__title__EtnHK')]"
+locators_xpath_20 = "//img[4]"
+locators_xpath_21 = "//div[@class='reviews-slider_title__qPuV_']"
+locators_xpath_22 = ("(//*[contains(@class,'footer_upSection__socialsWrapper__oV5Ne')]/descendant::div[@class='footer_"
+                     "upSection__socialItem__gtUYM'])[1]")
+locators_xpath_23 = ("//*[contains(@class,'footer_upSection__socialsWrapper__oV5Ne')]|//img[@src='/_next/static/media/"
+                     "mail.9af4e85e.svg']")
+locators_xpath_24 = "//*[contains(@class,'footer_gradientLine__pYizo')]"
+locators_xpath_25 = "(//a[@href='mailto:resiliencehub.ua@gmail.com'])[last()-1]"
+locators_xpath_26 = "(//a[@href='mailto:resiliencehub.ua@gmail.com'])[last()-1]/.."
+locators_xpath_27 = ("(//a[@href='mailto:resiliencehub.ua@gmail.com'])[last()-1]/ancestor::div[@class='footer_"
+                     "upSection__socialItem__text__DjX3C']")
+locators_xpath_28 = ("(//a[@href='mailto:resiliencehub.ua@gmail.com'])[last()-1]/ancestor::div[@class='footer_"
+                     "upSection__socialItem__text__DjX3C']/ancestor::div[@class='footer_upSection__socialItem__gtUYM']")
+locators_xpath_29 = "//div[@class='footer_downSection__logoItem__62_7q']"
+locators_xpath_30 = "//div[@class='footer_downSection__logoItem__62_7q']/ancestor::div[3]"
+locators_xpath_31 = "(//*[@id='clip0_948_32608'])[1]"
+
+locators_css_1 = ".footer_downSection__logoItem__62_7q"
+locators_css_2 = ".footer_downSection__logoItem__62_7q svg"
+locators_css_3 = "#__next"
+locators_css_4 = "div.index_faqScreen__title__Y_iDs"
+locators_css_5 = "*.footer_upSection__title__EtnHK"
+locators_css_6 = "div>img.index_startScreen__bg_desc__fC5vx"
+locators_css_7 = "img.index_startScreen__bg_desc__fC5vx"
+locators_css_8 = "[class=reviews-slider_title__qPuV_]"
+locators_css_9 = "[class*='views-slider_title__qPuV_']"
+locators_css_10 = ".footer_downSection__menuItem__qpAdc:last-child"
